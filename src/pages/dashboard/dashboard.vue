ae4ced65c74833234074046f9245282669cc72e4
sovlookup <gonorth@qq.com>
2022-09-10 22:23:21 +0800

    主页ok

--- src/pages/dashboard/dashboard.vue
+++ src/pages/dashboard/dashboard.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-const { setPageConfig } = usePageStore()
-
-onShow(() => {
-  setPageConfig({
-    pageTitle: 'Dashboard',
-  })
-})
+const phone = ref('')
+const sms = ref('')
 </script>
 
 <template>
-  <div class="p-6">
-    hello world
-  </div>
+  <van-cell-group inset class="pa-2">
+    <van-field v-model="phone" label="手机号" placeholder="请输入联系电话" />
+
+    <van-field v-model="sms" center clearable label="验证码" placeholder="请输入短信验证码">
+      <template #button>
+        <van-button size="small" type="primary">
+          获取验证码
+        </van-button>
+      </template>
+    </van-field>
+  </van-cell-group>
 </template>
